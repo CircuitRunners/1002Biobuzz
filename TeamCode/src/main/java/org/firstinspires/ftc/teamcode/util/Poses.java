8962b59a6ddbfd543f060341c0e288cb63c99286
James Liu <james.liu@circuitrunners.com>
2026-09-11 22:01:57 -0400

    testing branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Poses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Poses.java
@@ -2,14 +2,16 @@ package org.firstinspires.ftc.teamcode.util;
 
 import com.bylazar.configurables.annotations.Configurable;
 //import com.pedropathing.geometry.Pose;
+import com.pedropathing.api.PoseFactory;
 import com.pedropathing.math.Pose;
 
 @Configurable
 public class Poses {
-    public static final Pose startLine1 = new Pose(72, 72, Math.toRadians(90));
-    public static final Pose endLine1 = new Pose(72, 108, Math.toRadians(90));
-    public static final Pose curve1 = new Pose(108, 72, Math.toRadians(180));
-    public static final Pose curve1ControlPoint = new Pose(108, 108);
-    public static final Pose startLine2 = new Pose(108, 108, Math.toRadians(-90));
-    public static final Pose endLine2 = new Pose(108, 72, Math.toRadians(-90));
+    public static final PoseFactory p = PoseFactory.degrees();
+    public static final Pose startLine1 = p.of(72, 72, 90);
+    public static final Pose endLine1 = p.of(72, 108, 90);
+    public static final Pose curve1 = p.of(108, 72, 180);
+    public static final Pose curve1ControlPoint = p.of(108, 108, 0);
+    public static final Pose startLine2 = p.of(108, 108, -90);
+    public static final Pose endLine2 = p.of(108, 72, -90);
 }
